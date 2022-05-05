ab202b545103f24b3a3f131636769826cb44007a
Randy Fay <randy@randyfay.com>
2022-05-05 10:07:24 -0600

    Use CACHEBUST arg

--- demo.sh
+++ demo.sh
@@ -4,14 +4,14 @@ set -eu -o pipefail
 
 echo "Start time for date1=$(date)"
 pushd date1
-docker build -t date1 .
+docker build -t date1 --build-arg CACHEBUST=$(date +%s) .
 popd
 echo "Sleeping to let time pass"
 sleep 10
 echo "Start time for date2=$(date)"
 
 pushd date2
-docker build -t date2 .
+docker build -t date2 --build-arg CACHEBUST=$(date +%s) .
 popd
 
 docker run -it --rm date1 cat /date.txt
